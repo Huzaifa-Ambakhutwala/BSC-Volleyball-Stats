a29f9fc5149e3ddb5b26312552cb37b24069bc9a
Huzaifa Ambakhutwala <77138249+SOULHUZAIFA@users.noreply.github.com>
2025-05-14 16:25:59 +0000

    Added schedule page with google sheets iframe

--- client/src/pages/schedule/SchedulePage.tsx
+++ client/src/pages/schedule/SchedulePage.tsx
@@ -6,7 +6,7 @@ const SchedulePage: React.FC = () => {
             <h1 className="text-2xl font-bold mb-4">Match Schedule</h1>
             <div className="w-full h-[800px]">
                 <iframe
-                    src="https://bit.ly/bsc-1446-tball-live"
+                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRiw02fcdAFEq7yl-C9OwmhpKuTl43-BEdOc4JFUfKSZhGKo7G6r3DHXWlhNV3zYylUGZTIQmCpWWoF/pubhtml?widget=true&amp;headers=false"
                     className="w-full h-full border-0"
                     title="Match Schedule"
                     allowFullScreen
